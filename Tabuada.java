f295c754855b5d2d23198d551954fc904db49ee4
raphaluigi <56401545+raphaluigi@users.noreply.github.com>
2019-10-10 13:23:14 -0300

    Create Tabuada.java

---
+++ Tabuada.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Tabuada {
+    
+    public static void main (String[]Args) {
+    
+        Scanner entrada = new Scanner(System.in);
+        
+        int valor, i =1;
+        
+        valor= entrada.nextInt();
+        
+        while (i<=10) {
+            
+            System.out.println(valor* i);
+            
+            i++;
+            
+        }
